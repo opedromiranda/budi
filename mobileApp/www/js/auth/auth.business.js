443d7e917d4c66dcf03b8465390c2959d6b9f9db
Mike Pinto <msp.univ.por@gmail.com>
2014-12-11 09:56:36 +0000

    fix stuff

--- mobileApp/www/js/auth/auth.business.js
+++ mobileApp/www/js/auth/auth.business.js
@@ -48,8 +48,7 @@
             // Get User Info from Facebook
             var fb_user_info = $facebookLS.getUser();
 
-            console.log("ID", new_id);
-            fb_user_info.id = new_id;
+            fb_user_info._id = new_id;
             // Get Profile Picture
             var fb_user_picture;
             $authAdapter.getUserPicture()
