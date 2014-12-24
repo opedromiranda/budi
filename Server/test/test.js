8067ec15160a1ddcfb3459d55b12ff11e1207edc
Pedro Tavares <p.miguel.tr@gmail.com>
2014-12-24 17:19:44 +0000

    added tests

--- Server/test/test.js
+++ Server/test/test.js
@@ -1,6 +1,10 @@
 var expect = require('chai').expect;
 var assert = require("assert");
+var chai = require('chai');
+var sinon = require('sinon');
 
+var Budi = require('../models/budi');
+var Meet = require('../models/meet');
 
 expect(4+5).equal(9);
 expect(4+5).to.not.equal(10);
@@ -13,4 +17,34 @@ describe('Array', function(){
             assert.equal(-1, [1,2,3].indexOf(0));
         })
     })
+});
+
+describe('Budi', function() {
+    describe('findFriends', function () {
+        it('should return budi\'s friends IDs', function () {
+                var b = new Budi();
+                b.old_budis = [{
+                    id:1,
+                    friend: true
+                }, {
+                    id:2,
+                    friend: true
+                }, {
+                    id:3,
+                    friend: false
+                }];
+
+                var friends = b.findFriends();
+                assert.equal(friends[0], 1);
+                assert.equal(friends[1], 2);
+                assert.equal(friends.length, 2);
+        });
+
+        it('should query Meet to find a Meet for a budi', function () {
+            var b = new Budi();
+            Meet.find = sinon.spy();
+            b.findMeet();
+            expect(Meet.find.called);
+        });
+    });
 });
